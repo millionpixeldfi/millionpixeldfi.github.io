da138d163d187acfa7effbf0609d32af0642cfce
millionpixeldfi <millionpixeldfi@gmail.com>
2023-08-30 20:59:12 +0200

    Deployment

---
+++ assets/tag-0208d72f.js
@@ -0,0 +1,1 @@
+import{p as t}from"./index-9ba50553.js";const r=t({class:[String,Array],style:{type:[String,Array,Object],default:null}},"component"),o=t({tag:{type:String,default:"div"}},"tag");export{o as a,r as m};
